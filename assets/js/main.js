6d28947f20057bd99949861397527965ebadbde7
SuperCoder <supercoder@AirvonSperCoder.local>
2023-09-28 11:46:11 +0200

    charakter werden gecheckd ob die in Alphabet sind

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,24 +5,33 @@ const result= document.querySelector("#result")
 
 
 const encoder = () => {
-    let inputtext= (text.value).replaceAll(" ","");
+    let inputtext= ((text.value).replaceAll(" ","").toUpperCase());
     let caesar="";
+    let charAscii;
     numKey= Number(key.value);
     console.log(numKey);
     for (const char of inputtext) {
-        caesar+= String.fromCharCode((char.charCodeAt(0))+numKey)
+        charAscii = (char.charCodeAt(0))+numKey;
+        if(charAscii > 90){
+            charAscii= charAscii % 90 + 64 ;
+        }
+        caesar+= String.fromCharCode((charAscii));
     }
-    result.innerHTML= caesar.toUpperCase();
+    result.innerHTML= caesar;
 }
 
 const decoder = () => {
     let inputtext= (text.value).replaceAll(" ","");
     let caesar="";
+    let charAscii;
     numKey= Number(key.value);
-    console.log(numKey);
     for (const char of inputtext) {
-        console.log((char.charCodeAt(0))+numKey);
-        caesar+= String.fromCharCode((char.charCodeAt(0))-numKey)
+        console.log((char.charCodeAt(0))-numKey);
+        charAscii = (char.charCodeAt(0))-numKey;
+        if(charAscii < 65){
+            charAscii= charAscii % 90 + 64 ;
+        }
+        caesar+= String.fromCharCode((charAscii));
         console.log(caesar);
     }
     result.innerHTML= caesar.toLowerCase();
