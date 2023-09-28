5cca5ed4325c9518e4fa86c37a09da865ff05bb8
SuperCoder <supercoder@AirvonSperCoder.local>
2023-09-28 13:24:06 +0200

    encoder und decoder für grössere Schlüsselwert sind  koregiert

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,12 +8,13 @@ const encoder = () => {
     let inputtext= ((text.value).replaceAll(" ","").toUpperCase());
     let caesar="";
     let charAscii;
-    numKey= Number(key.value);
-    console.log(numKey);
+    numKey= Number(key.value) % 26;
+    // console.log(numKey);
     for (const char of inputtext) {
         charAscii = (char.charCodeAt(0))+numKey;
         if(charAscii > 90){
-            charAscii= charAscii - 90 + 64 ;
+            console.log("aaaa"+ charAscii);
+            charAscii= charAscii -26 ;
         }
         caesar+= String.fromCharCode((charAscii));
     }
@@ -21,15 +22,15 @@ const encoder = () => {
 }
 
 const decoder = () => {
-    let inputtext= (text.value).replaceAll(" ","");
+    let inputtext= ((text.value).replaceAll(" ","")).toUpperCase();
     let caesar="";
     let charAscii;
-    numKey= Number(key.value);
+    numKey= Number(key.value) % 26;
     for (const char of inputtext) {
         console.log((char.charCodeAt(0))-numKey);
         charAscii = (char.charCodeAt(0))-numKey;
         if(charAscii < 65 ){
-            charAscii= 91 - (65 - charAscii);
+            charAscii= charAscii + 26;
         }
         caesar+= String.fromCharCode((charAscii));
         console.log(caesar);
